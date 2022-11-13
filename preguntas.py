9b6c457765868af0ca71387bcd3ba229ebe51401
emhernandezva <emhernandezva@unal.edu.co>
2022-11-13 09:32:03 -0500

    Pregunta 1 V1.0

--- preguntas.py
+++ preguntas.py
@@ -23,7 +23,7 @@ def pregunta_01():
 
     # Cree un objeto de tipo `PolynomialFeatures` con grado `2`
     poly = PolynomialFeatures(degree=2)
-
+    poly.fit(data[["x"]])
     # Transforme la columna `x` del dataset `data` usando el objeto `poly`
     x_poly = poly.transform(data[["x"]])
 
